fa2a947deffe6a870ed2a3ecbe0fb865feb001b0
fernandom721 <00076617@uca.edu.sv>
2019-04-05 11:05:18 -0600

    se creo el fetch movie

--- app/src/main/java/com/example/pelispedia/MainActivity.kt
+++ app/src/main/java/com/example/pelispedia/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.pelispedia
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initRecycleView()
     }
 
     fun initRecycleView(){
@@ -26,6 +32,39 @@ class MainActivity : AppCompatActivity() {
             layoutManager = viewManager
             //ADAPTER
             adapter = movieAdapter
+        }
+
+
+    }
+
+    fun  addMovieToList(movie: Movie){
+        movieList.add(movie)
+        movieAdapter.changeList(movieList)
+        Log.d("Numbe", movieList.size.toString())
+    }
+
+    private inner class FetchMovie : AsyncTask<String, Void, String>(){
+        override fun doInBackground(vararg params: String): String {
+            if (params.isNullOrEmpty())return ""
+
+            val movieName = params[0]
+            val movieUrl = NetworkUtils()?.buildSearchUrl(movieName)
+
+            return try {
+                NetworkUtils().getResponseFromHttpUrl(movieUrl)
+            }catch (e: IOException){
+                ""
+            }
+        }
+
+        override fun onPostExecute(movieInfo: String) {
+            super.onPostExecute(movieInfo)
+            if (!movieInfo.isEmpty()){
+                val movieJson = JSONObject(movieInfo)
+                if (movieJson.getString("Response") == "True"){
+
+                }
+            }
         }
 
     }
